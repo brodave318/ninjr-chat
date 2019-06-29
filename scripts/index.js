52eebd1ec6cb595935c0069edc30f143fbe5bcd3
brodave318 <brodave318@gmail.com>
2019-06-28 18:12:51 -0700

    Get new chats

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,15 @@
 // dom quieries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+// add new chat
+newChatForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom.addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.err(err));
+});
 
 // class instances
 const chatUI = new ChatUI(chatList);
